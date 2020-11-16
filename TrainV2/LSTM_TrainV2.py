4a67a7b5885f60b09f320b285d34ff00fe47fc58
vietsaclo <vietsaclo@gmail.com>
2020-11-16 22:05:52 +0700

    16/11/2020 toi uu lai code

--- TrainV2/LSTM_TrainV2.py
+++ TrainV2/LSTM_TrainV2.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         - Truy cap vao thu muc DIR_INPUT_TRAIN lay toan bo file video va nhan bo vao mang.
         - Vidu: names[da1_1_111_001.avi,nt2_1_222_002.avi] ~ labels[ [1,0,0], [0,1,0] ]
     '''
-    names, labels = cf.fun_getVideoLabelNames_EachFolder(path=cf.DIR_INPUT_TRAIN)
+    names, labels = cf.getVideoLabelNames_EachFolder(path=cf.DIR_INPUT_TRAIN)
 
     '''
         CHUAN BI TAP DU LIEU & NHAN DE TRAIN LSTM,
@@ -20,19 +20,19 @@ if __name__ == '__main__':
         Moi hinh duoc cho qua VGG16 de lay mau ~ 4096
         - Vidu: 20 frame (224 x 224) ~ 20 * 4096 = [ [4096PhanTu],... [4096PhanTu] ]
     '''
-    trainSet, labelSet = cf.fun_getTrainSet_LabelSet(numItem=len(names), modelVGG16= modelVGG16, names= names, labels= labels)
+    trainSet, labelSet = cf.getTrainSet_LabelSet(numItem=len(names), modelVGG16= modelVGG16, names= names, labels= labels)
 
     '''
         LAY MODEL LSTM DUOC DINH NGHIA,
         - Chi tiet dinh nghia mang LSTM tai config file
     '''
-    modelLSTM = cf.fun_getModelLSTM(num_classify= cf.NUM_CLASSIFY)
+    modelLSTM = cf.getModelLSTM(num_classify= cf.NUM_CLASSIFY)
     modelLSTM.summary()
 
     '''
         BAT DAU HUAN LUYEN LSTM
     '''
-    history = cf.fun_START_TRAINT_LSTM(modelVGG16= modelVGG16 ,modelLSTM= modelLSTM, trainSet= trainSet, labelSet= labelSet)
+    history = cf.fun_START_TRAINT_LSTM(modelLSTM= modelLSTM, trainSet= trainSet, labelSet= labelSet)
 
     '''
         SAVE MODEL LSTM VAO O DIA
